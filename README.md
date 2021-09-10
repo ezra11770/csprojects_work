a8f3be18267af65be55c024355b22664ef3ebec8
Eyre22 <ezraeyre22@marlborough.org>
2021-09-10 13:25:39 -0700

    something

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csprojects_work
+
+[live site] (https://ezra11770.github.io/csprojects_work/hello_p5/hello_p5.html)
--- hello_p5/hello_p5.js
+++ hello_p5/hello_p5.js
@@ -1,15 +1,41 @@
 function setup() {
   createCanvas(400, 400);
+  background(0);
 }
 
-let x = 50;
+let x = 200;
+let y = 200;
+let vx = 1;
+let vy = 2;
+let w = 0;
+let h = 0;
+let c = (random(255), random(255), random(255));
 
 function draw() {
-  background(0);
-  fill(255,0,0);
-  ellipse(x, 200, 100, 50);
-  x++;
 
-  if (x > width+50)
-    x = -50;
+  for (let i = 0; i < 10; i++) {
+  stroke(c)
+  fill(0);
+  ellipse(200, 200, w, h);
+  h++;
+  w++;
+  }
+
+
+
+x += vx;
+y += vy;
+
+if (x + 25 > width || x - 25 < 0)
+  vx *= -1;
+
+if (y + 25 > height || y - 25 < 0)
+  vy *= -1;
+
+}
+
+
+
+function mousePressed() {
+
 }
